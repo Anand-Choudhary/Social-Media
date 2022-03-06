ea085e987e9f835e1fb56a79b43f2aa61079efb3
Anand-Choudhary <anand.c.rishra@gmail.com>
2022-03-06 18:42:45 +0530

    delete and get user api

--- api/routes/users.js
+++ api/routes/users.js
@@ -71,17 +71,32 @@ router.put('/:id',async (req,res)=>{
     }
 })
 
-router.delete('/:id',async (req,res)=>{
+router.delete('/deleteaccount/:id',async (req,res)=>{
     if(req.body.userId === req.params.id)
     {
         try {
-            const user = await User.findByIdAndDelete(req.params.id,{
-                
-            })
+            const user = await User.findByIdAndDelete(req.params.id)
+            return res.status(200).json({
+                message: "Account Deleted"
+            });
         } catch (error) {
-            
+            return res.status(500).json(error);
         }
     }
+    else{
+        return res.status(500).json("You can only delete your account")
+    }
+})
+
+router.get('/:id',async (req,res)=>{
+    
+    try {
+        const user = await User.findById(req.params.id)
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+    
 })
 
 module.exports = router;
